764192171370dd64724610d54a92147f7f19e3e1
Fenniless <fenniless@gmail.com>
2019-03-06 17:57:03 -0500

    werkin

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
@@ -1,4 +1,12 @@
 package com.zipcodewilmington.froilansfarm;
 
+import java.util.List;
+
 public class Farm {
+    private List<Stable> stables;
+    private List<ChickenCoop> chickenCoops;
+    private Farmhouse farmhouse;
+
+
+
 }
---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farmer.java
@@ -0,0 +1,16 @@
+package com.zipcodewilmington.froilansfarm;
+
+public class Farmer extends Person implements Botanist, Eater, Rider{
+    public void plants() {
+
+    }
+
+    public void mount(Rideable obj) {
+
+    }
+
+    public void dismount(Rideable obj) {
+
+    }
+
+}
